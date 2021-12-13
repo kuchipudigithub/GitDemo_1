9e022ff9e12fbf38715cbea63a586f38399fa477
kuchipudigithub <kskqat@gmail.com>
2021-12-13 16:39:35 +1100

    pushb3 push 4:39

--- src/main/java/Course/EbayApp_Emulator.java
+++ src/main/java/Course/EbayApp_Emulator.java
@@ -53,6 +53,9 @@ public class EbayApp_Emulator {
 	       driver.findElementById("com.ebay.mobile:id/button_register9").click();
 	       driver.findElementById("com.ebay.mobile:id/button_register10").click();
 	       driver.findElementById("com.ebay.mobile:id/button_register11").click();
+	       //branch 3 push--
+	       driver.findElementById("com.ebay.mobile:id/button_register12").click();
+	       
 	       // create buss account
 	       
 	       
